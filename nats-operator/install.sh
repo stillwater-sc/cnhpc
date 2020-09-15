98fd852d8337f4dc444f33bbdbfc44eb9b1dd1a2
Ravenwater <theo@stillwater-sc.com>
2020-09-15 14:34:40 -0400

    making install local

--- nats-operator/install.sh
+++ nats-operator/install.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-kubectl apply -f https://github.com/nats-io/nats-operator/releases/download/v0.7.4/00-prereqs.yaml
-kubectl apply -f https://github.com/nats-io/nats-operator/releases/download/v0.7.4/10-deployment.yaml
+kubectl apply -f ./v0.7.4/00-prereqs.yaml
+kubectl apply -f ./v0.7.4/10-deployment.yaml
+
+# kubectl apply -f https://github.com/nats-io/nats-operator/releases/download/v0.7.4/00-prereqs.yaml
+# kubectl apply -f https://github.com/nats-io/nats-operator/releases/download/v0.7.4/10-deployment.yaml
 
 # docker image
 # docker run connecteverything/nats-operator:0.7.4
