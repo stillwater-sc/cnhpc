9866bb4d4195fdc19291286f3b5ddb4b946d2b28
Ravenwater <theo@stillwater-sc.com>
2020-09-15 14:33:07 -0400

    adding NATS operator

---
+++ nats-operator/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+kubectl apply -f https://github.com/nats-io/nats-operator/releases/download/v0.7.4/00-prereqs.yaml
+kubectl apply -f https://github.com/nats-io/nats-operator/releases/download/v0.7.4/10-deployment.yaml
+
+# docker image
+# docker run connecteverything/nats-operator:0.7.4
+
+
