a81299d7ad0533498476a36aba2ebb639fd2ad49
Ravenwater <theo@stillwater-sc.com>
2020-09-20 08:15:57 -0400

    creating docker hub secret configuration

--- apps/picalc/tekton/create-secrets.sh
+++ apps/picalc/tekton/create-secrets.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # IBM Cloud CR
-kubectl create secret generic my-registry-secret --type="kubernetes.io/basic-auth" --from-literal=username=iamapikey --from-literal=password=mykey
-kubectl annotate secret my-registry-key tekton.dev/docker-0=<domainname-of-registry>
+# kubectl create secret generic my-registry-secret --type="kubernetes.io/basic-auth" --from-literal=username=iamapikey --from-literal=password=mykey
+# kubectl annotate secret my-registry-key tekton.dev/docker-0=<domainname-of-registry>
 
 # Docker Hub CR
-kubectl create secret generic my-registry-secret --type="kubernetes.io/basic-auth" --from-literal=username=stillwater --from-literal=password=mykey
+kubectl create secret generic my-registry-secret --type="kubernetes.io/basic-auth" --from-literal=username=stillwater --from-literal=password=$DOCKER_HUB_TOKEN
 kubectl annotate secret my-registry-key tekton.dev/docker-0=hub.docker.com
