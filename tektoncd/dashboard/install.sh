60a7111e0da8e9b53667d8df79700faf61d86783
Ravenwater <theo@stillwater-sc.com>
2020-09-24 18:28:52 -0400

    adding install script for tekton dashboard

---
+++ tektoncd/dashboard/install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# kubectl apply --filename https://github.com/tektoncd/dashboard/releases/latest/download/tekton-dashboard-release.yaml
+kubectl apply -f tekton-dashboard-release.yaml
